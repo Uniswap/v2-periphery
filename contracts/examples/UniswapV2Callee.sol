b693cf7bbc09b85864ed98c237960823ebc9f57b
Noah Zinsmeister <noahwz@gmail.com>
2020-03-10 17:33:14 -0400

    improve callee example

--- contracts/examples/UniswapV2Callee.sol
+++ contracts/examples/UniswapV2Callee.sol
@@ -1,22 +1,20 @@
 pragma solidity =0.5.16;
 
 import '../interfaces/V2/IUniswapV2Callee.sol';
+import '../UniswapV2Helper.sol';
 import '../interfaces/V2/IUniswapV2Factory.sol';
 import '../interfaces/V2/IUniswapV2Exchange.sol';
 
-contract UniswapV2Callee is IUniswapV2Callee {
-    // factory address is identical across mainnet and testnets but differs between testing and deployed environments
-    IUniswapV2Factory public constant factory = IUniswapV2Factory(0xdCCc660F92826649754E357b11bd41C31C0609B9);
-
+contract UniswapV2Callee is IUniswapV2Callee, UniswapV2Helper {
     function uniswapV2Call(
-        address,       /* sender  */
+        address,       /* sender */
         uint256,       /* amount0 */
         uint256,       /* amount1 */
-        bytes calldata /* data    */
+        bytes calldata /* data */
     ) external {
         address token0 = IUniswapV2Exchange(msg.sender).token0();
         address token1 = IUniswapV2Exchange(msg.sender).token1();
-        require(factory.getExchange(token0, token1) == msg.sender, 'FORBIDDEN');
-        // execute arbitrary logic here 💥
+        assert(msg.sender == exchangeFor(token0, token1));
+        // execute arbitrary logic here
     }
 }
