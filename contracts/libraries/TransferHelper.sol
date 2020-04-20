ab370afc82abd131823fe08697c7bf93a2d382bc
Noah Zinsmeister <noahwz@gmail.com>
2020-04-20 13:21:49 -0400

    use new value syntax

--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -21,7 +21,7 @@ library TransferHelper {
     }
 
     function safeTransferETH(address to, uint value) internal {
-        (bool success,) = to.call.value(value)(new bytes(0));
+        (bool success,) = to.call{value: value}(new bytes(0));
         require(success, 'TransferHelper: ETH_TRANSFER_FAILED');
     }
 }
