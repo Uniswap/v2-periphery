b297c13f3163ae378b4e416236c937d8c8681842
Noah Zinsmeister <noahwz@gmail.com>
2020-04-20 13:00:05 -0400

    add constructor back

--- contracts/UniswapV2Migrator.sol
+++ contracts/UniswapV2Migrator.sol
@@ -11,6 +11,11 @@ contract UniswapV2Migrator is IUniswapV2Migrator {
     IUniswapV1Factory immutable factoryV1;
     IUniswapV2Router01 immutable router;
 
+    constructor(address _factoryV1, address _router) public {	
+        factoryV1 = IUniswapV1Factory(_factoryV1);	
+        router = IUniswapV2Router01(_router);	
+    }
+
     // needs to accept ETH from any v1 exchange and the router. ideally this could be enforced, as in the router,
     // but it's not possible because it requires a call to the v1 factory, which takes too much gas
     receive() external payable {}
