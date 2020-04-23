c5ce8951da7d8785af8ef76ef9679e8af8ca33c8
Moody Salem <moodysalem@users.noreply.github.com>
2020-04-23 09:59:15 -0500

    Use @uniswap/lib and simplify example oracle (#15)
    * Use @uniswap/lib and simplify example oracle
    
    * Assert => require
    Improve some documentation
    
    * Remove pair.sync in favor of counterfactual
    
    * Add a disclaimer

--- contracts/ExampleOracleSimple.sol
+++ contracts/ExampleOracleSimple.sol
@@ -2,11 +2,12 @@ pragma solidity =0.6.6;
 
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+import '@uniswap/lib/contracts/libraries/FixedPoint.sol';
 
-import './libraries/UQ112x112.sol';
-
+// fixed window oracle that recomputes the average price for the entire period once every period
+// note that the price average is only guaranteed to be over at least 1 period, but may be over a longer period
 contract ExampleOracleSimple {
-    using UQ112x112 for uint224;
+    using FixedPoint for *;
 
     uint public constant PERIOD = 24 hours;
 
@@ -17,8 +18,8 @@ contract ExampleOracleSimple {
     uint    public price0CumulativeLast;
     uint    public price1CumulativeLast;
     uint32  public blockTimestampLast;
-    uint224 public price0Average;
-    uint224 public price1Average;
+    FixedPoint.uq112x112 public price0Average;
+    FixedPoint.uq112x112 public price1Average;
 
     constructor(address factory, address tokenA, address tokenB) public {
         IUniswapV2Pair _pair = IUniswapV2Pair(IUniswapV2Factory(factory).getPair(tokenA, tokenB));
@@ -30,30 +31,33 @@ contract ExampleOracleSimple {
         uint112 reserve0;
         uint112 reserve1;
         (reserve0, reserve1, blockTimestampLast) = _pair.getReserves();
-        assert(reserve0 != 0 && reserve1 != 0); // ensure that there's liquidity in the pair
-        assert(blockTimestampLast != 0); // ensure there's a price history
+        require(reserve0 != 0 && reserve1 != 0, 'ExampleOracleSimple: NO_RESERVES'); // ensure that there's liquidity in the pair
+        require(blockTimestampLast != 0, 'ExampleOracleSimple: NO_PRICE_HISTORY'); // ensure there's a price history
     }
 
     function update() external {
         uint32 blockTimestamp = uint32(block.timestamp % 2**32);
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
-        assert(timeElapsed >= PERIOD); // ensure that at least one full period has passed since the last update
+
+        // ensure that at least one full period has passed since the last update
+        require(timeElapsed >= PERIOD, 'ExampleOracleSimple: PERIOD_NOT_ELAPSED');
 
         uint price0Cumulative = pair.price0CumulativeLast();
         uint price1Cumulative = pair.price1CumulativeLast();
 
         // if time has elapsed since the last update on the pair, mock the accumulated price values
+        // alternately, call pair.sync() before reading the cumulative last price from the pair
         (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLastFromPair) = pair.getReserves();
-        assert(reserve0 != 0 && reserve1 != 0); // ensure that there's still liquidity in the pair
         if (blockTimestampLastFromPair != blockTimestamp) {
             uint timeElapsedPartial = blockTimestamp - blockTimestampLastFromPair; // overflow is desired
-            price0Cumulative += uint(UQ112x112.encode(reserve1).uqdiv(reserve0)) * timeElapsedPartial; // counterfactual
-            price1Cumulative += uint(UQ112x112.encode(reserve0).uqdiv(reserve1)) * timeElapsedPartial; // counterfactual
+            price0Cumulative += uint(FixedPoint.fraction(reserve1, reserve0)._x) * timeElapsedPartial; // counterfactual
+            price1Cumulative += uint(FixedPoint.fraction(reserve0, reserve1)._x) * timeElapsedPartial; // counterfactual
         }
 
-        // - overflow is desired, / never overflows, casting never truncates
-        price0Average = uint224((price0Cumulative - price0CumulativeLast) / timeElapsed);
-        price1Average = uint224((price1Cumulative - price1CumulativeLast) / timeElapsed);
+        // overflow is desired, casting never truncates
+        // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
+        price0Average = FixedPoint.uq112x112(uint224((price0Cumulative - price0CumulativeLast) / timeElapsed));
+        price1Average = FixedPoint.uq112x112(uint224((price1Cumulative - price1CumulativeLast) / timeElapsed));
 
         price0CumulativeLast = price0Cumulative;
         price1CumulativeLast = price1Cumulative;
@@ -62,10 +66,10 @@ contract ExampleOracleSimple {
 
     function consult(address token, uint amountIn) external view returns (uint amountOut) {
         if (token == token0) {
-            amountOut = UQ112x112.decode(price0Average.uqmul(amountIn));
+            amountOut = price0Average.mul(amountIn).decode144();
         } else {
-            assert(token == token1);
-            amountOut = UQ112x112.decode(price1Average.uqmul(amountIn));
+            require(token == token1, 'ExampleOracleSimple: INVALID_TOKEN');
+            amountOut = price1Average.mul(amountIn).decode144();
         }
     }
 }
--- contracts/UniswapV2Router01.sol
+++ contracts/UniswapV2Router01.sol
@@ -1,12 +1,12 @@
 pragma solidity =0.6.6;
 
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
+import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
 
 import './interfaces/IUniswapV2Router01.sol';
 import './UniswapV2Library.sol';
 import './interfaces/IERC20.sol';
 import './interfaces/IWETH.sol';
-import './libraries/TransferHelper.sol';
 
 contract UniswapV2Router01 is IUniswapV2Router01, UniswapV2Library {
     address public immutable override WETH;
