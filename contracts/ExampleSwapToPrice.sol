6ee04c2f304a7c254a14d13b10b7eeec9d3d889b
Moody Salem <moody.salem@gmail.com>
2020-04-27 11:56:48 -0400

    Remove duplicate library

--- contracts/ExampleSwapToPrice.sol
+++ contracts/ExampleSwapToPrice.sol
@@ -3,7 +3,6 @@ pragma solidity =0.6.6;
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 import '@uniswap/lib/contracts/libraries/Babylonian.sol';
 import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
-import '@uniswap/lib/contracts/libraries/Babylonian.sol';
 
 import './interfaces/IERC20.sol';
 import './interfaces/IUniswapV2Router01.sol';
