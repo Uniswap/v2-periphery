bd0dc48759ff1814341cdcd5f46a5f57c4147aa6
Noah Zinsmeister <noahwz@gmail.com>
2020-05-28 14:53:14 -0400

    fix gas test for new router02 usage

--- test/ExampleSwapToPrice.spec.ts
+++ test/ExampleSwapToPrice.spec.ts
@@ -192,7 +192,7 @@ describe('ExampleSwapToPrice', () => {
         overrides
       )
       const receipt = await tx.wait()
-      expect(receipt.gasUsed).to.eq('122307')
+      expect(receipt.gasUsed).to.eq('129555')
     }).retries(2) // gas test is inconsistent
   })
 })
