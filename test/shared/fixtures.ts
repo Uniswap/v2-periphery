69617118cda519dab608898d62aaa79877a61004
Noah Zinsmeister <noahwz@gmail.com>
2020-06-05 16:05:57 -0400

    UniswapV2Router02 skeleton (#31)
    * add skeleton for router02
    
    * run 01 tests for 02 as well
    
    empty out 02 tests
    
    fixture always points to latest router
    
    * fix tests
    
    * actual fix this time
    
    * use latest router for all contracts that need it
    
    * user router01 in the migrator
    
    * Router02 proposal (#33)
    
    * add virtual to relevant functions
    
    private -> internal
    
    enforce line length
    
    * fix bug and add tests
    
    * Router02 additions to support FoT tokens (#34)
    
    * create skeleton UniswapV2Router03
    
    run all the appropriate tests for all the appropriate routers
    
    * add router capable of handling FoT tokens
    
    * explicitly check returned amounts array
    
    * add amounts tests for FoT tokens in 03
    
    * fix gas tests
    
    * put 03 changes into 02
    
    * remove unnecessary comment

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -13,6 +13,8 @@ import UniswapV1Exchange from '../../build/UniswapV1Exchange.json'
 import UniswapV1Factory from '../../build/UniswapV1Factory.json'
 import UniswapV2Router01 from '../../build/UniswapV2Router01.json'
 import UniswapV2Migrator from '../../build/UniswapV2Migrator.json'
+import UniswapV2Router02 from '../../build/UniswapV2Router02.json'
+import RouterEventEmitter from '../../build/RouterEventEmitter.json'
 
 const overrides = {
   gasLimit: 9999999
@@ -25,6 +27,9 @@ interface V2Fixture {
   WETHPartner: Contract
   factoryV1: Contract
   factoryV2: Contract
+  router01: Contract
+  router02: Contract
+  routerEventEmitter: Contract
   router: Contract
   migrator: Contract
   WETHExchangeV1: Contract
@@ -46,9 +51,15 @@ export async function v2Fixture(provider: Web3Provider, [wallet]: Wallet[]): Pro
   // deploy V2
   const factoryV2 = await deployContract(wallet, UniswapV2Factory, [wallet.address])
 
-  // deploy router and migrator
-  const router = await deployContract(wallet, UniswapV2Router01, [factoryV2.address, WETH.address], overrides)
-  const migrator = await deployContract(wallet, UniswapV2Migrator, [factoryV1.address, router.address], overrides)
+  // deploy routers
+  const router01 = await deployContract(wallet, UniswapV2Router01, [factoryV2.address, WETH.address], overrides)
+  const router02 = await deployContract(wallet, UniswapV2Router02, [factoryV2.address, WETH.address], overrides)
+
+  // event emitter for testing
+  const routerEventEmitter = await deployContract(wallet, RouterEventEmitter, [])
+
+  // deploy migrator
+  const migrator = await deployContract(wallet, UniswapV2Migrator, [factoryV1.address, router01.address], overrides)
 
   // initialize V1
   await factoryV1.createExchange(WETHPartner.address, overrides)
@@ -77,7 +88,10 @@ export async function v2Fixture(provider: Web3Provider, [wallet]: Wallet[]): Pro
     WETHPartner,
     factoryV1,
     factoryV2,
-    router,
+    router01,
+    router02,
+    router: router02, // the default router, 01 had a minor bug
+    routerEventEmitter,
     migrator,
     WETHExchangeV1,
     pair,
