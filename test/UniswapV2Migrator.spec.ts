dd17e8627ce11db20116667978be9c4337c0cbc7
AugustoL <me@augustol.com>
2020-06-13 20:22:43 -0300

    refactor(tests): Fix contracts imports with new contracts names and replace uniswap mentions for dxs

--- test/UniswapV2Migrator.spec.ts
+++ test/UniswapV2Migrator.spec.ts
@@ -15,7 +15,7 @@ const overrides = {
   gasLimit: 9999999
 }
 
-describe('UniswapV2Migrator', () => {
+describe('DXswapMigrator', () => {
   const provider = new MockProvider({
     hardfork: 'istanbul',
     mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
