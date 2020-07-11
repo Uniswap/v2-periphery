455c543ded42c76c92ce17bbcadda21328c16613
AugustoL <me@augustol.com>
2020-07-11 15:21:40 -0300

    feat(contracts): Change token pair symbol use for DXS symbol in PairNamer library

--- contracts/libraries/PairNamer.sol
+++ contracts/libraries/PairNamer.sol
@@ -2,9 +2,9 @@ pragma solidity >=0.5.0;
 
 import './SafeERC20Namer.sol';
 
-// produces names for pairs of tokens using Uniswap's naming scheme
+// produces names for pairs of tokens using DXswap's naming scheme
 library PairNamer {
-    string private constant TOKEN_SYMBOL_PREFIX = '🦄';
+    string private constant TOKEN_PAIR_PREFIX = 'DXS';
     string private constant TOKEN_SEPARATOR = ':';
 
     // produces a pair descriptor in the format of `${prefix}${name0}:${name1}${suffix}`
@@ -20,11 +20,12 @@ library PairNamer {
         );
     }
 
-    // produces a pair symbol in the format of `🦄${symbol0}:${symbol1}${suffix}`
+    // produces a pair symbol in the format of `DXS:${symbol0}:${symbol1}${suffix}`
     function pairSymbol(address token0, address token1, string memory suffix) internal view returns (string memory) {
         return string(
             abi.encodePacked(
-                TOKEN_SYMBOL_PREFIX,
+                TOKEN_PAIR_PREFIX,
+                TOKEN_SEPARATOR,
                 SafeERC20Namer.tokenSymbol(token0),
                 TOKEN_SEPARATOR,
                 SafeERC20Namer.tokenSymbol(token1),
