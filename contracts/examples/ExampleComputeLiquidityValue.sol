d9eaf67e72b2816560413618954538a53867917c
Moody Salem <moody.salem@gmail.com>
2020-08-21 13:01:32 -0500

    move to library, make gas tests

--- contracts/examples/ExampleComputeLiquidityValue.sol
+++ contracts/examples/ExampleComputeLiquidityValue.sol
@@ -1,97 +1,33 @@
 pragma solidity =0.6.6;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
-import '@uniswap/lib/contracts/libraries/Babylonian.sol';
-
-import '../libraries/SafeMath.sol';
-import '../libraries/UniswapV2Library.sol';
+import '../libraries/UniswapV2LiquidityMathLibrary.sol';
 
 contract ExampleComputeLiquidityValue {
     using SafeMath for uint256;
 
-    address public /*immutable*/ factory;
+    address public immutable factory;
 
     constructor(address factory_) public {
         factory = factory_;
     }
 
-    // computes the direction and magnitude of the profit-maximizing trade
-    function computeProfitMaximizingTrade(
-        uint256 truePriceTokenA,
-        uint256 truePriceTokenB,
-        uint256 reserveA,
-        uint256 reserveB
-    ) pure private returns (bool aToB, uint256 amountIn) {
-        aToB = reserveA.mul(truePriceTokenB) / reserveB < truePriceTokenA;
-
-        uint256 invariant = reserveA.mul(reserveB);
-
-        uint256 leftSide = Babylonian.sqrt(
-            invariant.mul(aToB ? truePriceTokenA : truePriceTokenB).mul(1000) /
-            uint256(aToB ? truePriceTokenB : truePriceTokenA).mul(997)
-        );
-        uint256 rightSide = (aToB ? reserveA.mul(1000) : reserveB.mul(1000)) / 997;
-
-        if (leftSide < rightSide) return (false, 0);
-        // compute the amount that must be sent to move the price to the profit-maximizing price
-        amountIn = leftSide.sub(rightSide);
-    }
-
-    // gets the reserves after an arbitrage moves the price to the profit-maximizing ratio given an externally observed true price
+    // see UniswapV2LiquidityMathLibrary#getReservesAfterArbitrage
     function getReservesAfterArbitrage(
         address tokenA,
         address tokenB,
         uint256 truePriceTokenA,
         uint256 truePriceTokenB
-    ) view private returns (uint256 reserveA, uint256 reserveB) {
-        // first get reserves before the swap
-        (reserveA, reserveB) = UniswapV2Library.getReserves(factory, tokenA, tokenB);
-
-        require(reserveA > 0 && reserveB > 0, 'ComputeLiquidityValue: ZERO_PAIR_RESERVES');
-
-        // then compute how much to swap to arb to the true price
-        (bool aToB, uint256 amountIn) = computeProfitMaximizingTrade(truePriceTokenA, truePriceTokenB, reserveA, reserveB);
-
-        if (amountIn == 0) {
-            return (reserveA, reserveB);
-        }
-
-        // now affect the trade to the reserves
-        if (aToB) {
-            uint amountOut = UniswapV2Library.getAmountOut(amountIn, reserveA, reserveB);
-            reserveA += amountIn;
-            reserveB -= amountOut;
-        } else {
-            uint amountOut = UniswapV2Library.getAmountOut(amountIn, reserveB, reserveA);
-            reserveB += amountIn;
-            reserveA -= amountOut;
-        }
+    ) external view returns (uint256 reserveA, uint256 reserveB) {
+        return UniswapV2LiquidityMathLibrary.getReservesAfterArbitrage(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB
+        );
     }
 
-    function getLiquidityValue(
-        uint256 reservesA,
-        uint256 reservesB,
-        uint256 totalSupply,
-        uint256 liquidityAmount,
-        bool feeOn,
-        uint kLast
-    ) private pure returns (uint256 tokenAAmount, uint256 tokenBAmount) {
-        if (feeOn && kLast > 0) {
-            uint rootK = Babylonian.sqrt(reservesA.mul(reservesB));
-            uint rootKLast = Babylonian.sqrt(kLast);
-            if (rootK > rootKLast) {
-                uint numerator = totalSupply.mul(rootK.sub(rootKLast));
-                uint denominator = rootK.mul(5).add(rootKLast);
-                uint feeLiquidity = numerator / denominator;
-                totalSupply = totalSupply.add(feeLiquidity);
-            }
-        }
-        return (reservesA.mul(liquidityAmount) / totalSupply, reservesB.mul(liquidityAmount) / totalSupply);
-    }
-
-    // given two tokens, tokenA and tokenB, and their "true price", i.e. the observed ratio of value of token A to token B,
-    // and a liquidity amount, returns the value of the liquidity in terms of tokenA and tokenB
+    // see UniswapV2LiquidityMathLibrary#getLiquidityValueAfterArbitrageToPrice
     function getLiquidityValueAfterArbitrageToPrice(
         address tokenA,
         address tokenB,
@@ -102,15 +38,35 @@ contract ExampleComputeLiquidityValue {
         uint256 tokenAAmount,
         uint256 tokenBAmount
     ) {
-        bool feeOn = IUniswapV2Factory(factory).feeTo() != address(0);
-        IUniswapV2Pair pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
-        uint kLast = feeOn ? pair.kLast() : 0;
-        uint totalSupply = pair.totalSupply();
+        return UniswapV2LiquidityMathLibrary.getLiquidityValueAfterArbitrageToPrice(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB,
+            liquidityAmount
+        );
+    }
 
-        require(totalSupply >= liquidityAmount && liquidityAmount > 0, 'ComputeLiquidityValue: LIQUIDITY_AMOUNT');
-
-        (uint reservesA, uint reservesB) = getReservesAfterArbitrage(tokenA, tokenB, truePriceTokenA, truePriceTokenB);
-
-        return getLiquidityValue(reservesA, reservesB, totalSupply, liquidityAmount, feeOn, kLast);
+    function getGasCostOfGetLiquidityValueAfterArbitrageToPrice(
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB,
+        uint256 liquidityAmount
+    ) external view returns (
+        uint256
+    ) {
+        uint gasBefore = gasleft();
+        UniswapV2LiquidityMathLibrary.getLiquidityValueAfterArbitrageToPrice(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB,
+            liquidityAmount
+        );
+        uint gasAfter = gasleft();
+        return gasBefore - gasAfter;
     }
 }
