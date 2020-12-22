dda62473e2da448bc9cb8f4514dadda4aeede5f4
Moody Salem <moodysalem@users.noreply.github.com>
2020-12-22 10:21:24 -0600

    Adds a contract to compute liquidity value after arbing to an observed price (#49)
    * add contracts and some basic tests
    
    * fix lint
    
    * move to library, make gas tests
    
    * fix pragma
    
    * add a get liquidity value function, refactor swap to price example
    
    * couple more tests
    
    * bump the uniswap lib dependency for cheaper sqrt and FullMath.mulDiv
    
    * use safemath for the mul
    
    * add a test for large numbers

--- contracts/examples/ExampleSwapToPrice.sol
+++ contracts/examples/ExampleSwapToPrice.sol
@@ -4,6 +4,7 @@ import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 import '@uniswap/lib/contracts/libraries/Babylonian.sol';
 import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
 
+import '../libraries/UniswapV2LiquidityMathLibrary.sol';
 import '../interfaces/IERC20.sol';
 import '../interfaces/IUniswapV2Router01.sol';
 import '../libraries/SafeMath.sol';
@@ -18,27 +19,6 @@ contract ExampleSwapToPrice {
     constructor(address factory_, IUniswapV2Router01 router_) public {
         factory = factory_;
         router = router_;
-    }
-
-    // computes the direction and magnitude of the profit-maximizing trade
-    function computeProfitMaximizingTrade(
-        uint256 truePriceTokenA,
-        uint256 truePriceTokenB,
-        uint256 reserveA,
-        uint256 reserveB
-    ) pure public returns (bool aToB, uint256 amountIn) {
-        aToB = reserveA.mul(truePriceTokenB) / reserveB < truePriceTokenA;
-
-        uint256 invariant = reserveA.mul(reserveB);
-
-        uint256 leftSide = Babylonian.sqrt(
-            invariant.mul(aToB ? truePriceTokenA : truePriceTokenB).mul(1000) /
-            uint256(aToB ? truePriceTokenB : truePriceTokenA).mul(997)
-        );
-        uint256 rightSide = (aToB ? reserveA.mul(1000) : reserveB.mul(1000)) / 997;
-
-        // compute the amount that must be sent to move the price to the profit-maximizing price
-        amountIn = leftSide.sub(rightSide);
     }
 
     // swaps an amount of either token such that the trade is profit-maximizing, given an external true price
@@ -63,11 +43,13 @@ contract ExampleSwapToPrice {
         uint256 amountIn;
         {
             (uint256 reserveA, uint256 reserveB) = UniswapV2Library.getReserves(factory, tokenA, tokenB);
-            (aToB, amountIn) = computeProfitMaximizingTrade(
+            (aToB, amountIn) = UniswapV2LiquidityMathLibrary.computeProfitMaximizingTrade(
                 truePriceTokenA, truePriceTokenB,
                 reserveA, reserveB
             );
         }
+
+        require(amountIn > 0, 'ExampleSwapToPrice: ZERO_AMOUNT_IN');
 
         // spend up to the allowance of the token in
         uint256 maxSpend = aToB ? maxSpendTokenA : maxSpendTokenB;
--- test/ExampleSwapToPrice.spec.ts
+++ test/ExampleSwapToPrice.spec.ts
@@ -192,7 +192,7 @@ describe('ExampleSwapToPrice', () => {
         overrides
       )
       const receipt = await tx.wait()
-      expect(receipt.gasUsed).to.eq('122329')
+      expect(receipt.gasUsed).to.eq('115129')
     }).retries(2) // gas test is inconsistent
   })
 })
