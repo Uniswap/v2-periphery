3432699d7838eee1151366630cba806eecc48ca3
lidagoudagou <843962249@qq.com>
2022-12-07 23:56:14 +0800

    UniswapV2Library注释-两个TODO

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -8,6 +8,7 @@ library UniswapV2Library {
     using SafeMath for uint;
 
     // returns sorted token addresses, used to handle return values from pairs sorted in this order
+    // 对token进行排序，低地址在高地址前
     function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
         require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
         (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
@@ -15,6 +16,7 @@ library UniswapV2Library {
     }
 
     // calculates the CREATE2 address for a pair without making any external calls
+    // 通过外部调用，获取pair的地址（tokenA与tokenB的池子地址）
     function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
         (address token0, address token1) = sortTokens(tokenA, tokenB);
         pair = address(uint(keccak256(abi.encodePacked(
@@ -26,6 +28,7 @@ library UniswapV2Library {
     }
 
     // fetches and sorts the reserves for a pair
+    // 获取恒定乘积的乘数，即资产池中剩余的数量
     function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
         (address token0,) = sortTokens(tokenA, tokenB);
         (uint reserve0, uint reserve1,) = IUniswapV2Pair(pairFor(factory, tokenA, tokenB)).getReserves();
@@ -33,6 +36,13 @@ library UniswapV2Library {
     }
 
     // given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
+    // 这里面计算出来的价格是在资产价格不波动的情况下算出来的。
+    /*
+        rA*rB=k
+        此时 aA*rA = aB*rB
+        => aB = aA*rA/rB
+    */
+    // TODO 计算出来的结果，跟实现相反，不是很理解。需要深究一下。
     function quote(uint amountA, uint reserveA, uint reserveB) internal pure returns (uint amountB) {
         require(amountA > 0, 'UniswapV2Library: INSUFFICIENT_AMOUNT');
         require(reserveA > 0 && reserveB > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
@@ -40,25 +50,39 @@ library UniswapV2Library {
     }
 
     // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
+    // 要输出的代币的reserve
+    /*
+        推导过程
+        rIn*rOut=k
+        （rIn+aIn）*（rOut-aOut）=k
+        rOut-aOut = k/(rIn+aIn)
+        aOut = rOut-k/(rIn+aIn)
+        aOut = (rOut*(rIn+aIn)-k)/(rIn+aIn)
+        aOut = aIn/(rIn+aIn)
+        上述推导为未扣除千分之三手续费的推导过程，下面代码实现支持了手续费。
+    */
     function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
         require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
-        uint amountInWithFee = amountIn.mul(997);
+        uint amountInWithFee = amountIn.mul(997);// 千分之3的手续费扣除
         uint numerator = amountInWithFee.mul(reserveOut);
         uint denominator = reserveIn.mul(1000).add(amountInWithFee);
         amountOut = numerator / denominator;
     }
 
     // given an output amount of an asset and pair reserves, returns a required input amount of the other asset
+    // 按照要换出多少，算出要要输入多少。
     function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) internal pure returns (uint amountIn) {
         require(amountOut > 0, 'UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
         uint numerator = reserveIn.mul(amountOut).mul(1000);
         uint denominator = reserveOut.sub(amountOut).mul(997);
-        amountIn = (numerator / denominator).add(1);
+        amountIn = (numerator / denominator).add(1);// TODO 为什么要+1，根据无手续费的情况下，推出来的结果不需要+1.
     }
 
     // performs chained getAmountOut calculations on any number of pairs
+    // 链式交易中的数据
+    // a->b->c，返回a输入后，给出多少b（交易中间态），与c。最终c的数量是换出来的c数量。
     function getAmountsOut(address factory, uint amountIn, address[] memory path) internal view returns (uint[] memory amounts) {
         require(path.length >= 2, 'UniswapV2Library: INVALID_PATH');
         amounts = new uint[](path.length);
@@ -70,6 +94,7 @@ library UniswapV2Library {
     }
 
     // performs chained getAmountIn calculations on any number of pairs
+    // 与上一个方法类似，是记录要输入的以及中间要输入的资产数量。
     function getAmountsIn(address factory, uint amountOut, address[] memory path) internal view returns (uint[] memory amounts) {
         require(path.length >= 2, 'UniswapV2Library: INVALID_PATH');
         amounts = new uint[](path.length);
